2b558ab0afa7d67d1945d230e4ab989c482e0128
Francis Chong <francis@ignition.hk>
2016-02-27 16:36:20 +0800

    Add a universal framework target

---
+++ KxSMB_Framework/KxSMB_Framework.h
@@ -0,0 +1,17 @@
+//
+//  KxSMB_Framework.h
+//  KxSMB_Framework
+//
+//  Created by Chan Fai Chong on 27/2/2016.
+//  Copyright © 2016 Konstantin Bukreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KxSMB_Framework.
+FOUNDATION_EXPORT double KxSMB_FrameworkVersionNumber;
+
+//! Project version string for KxSMB_Framework.
+FOUNDATION_EXPORT const unsigned char KxSMB_FrameworkVersionString[];
+
+#import <KxSMB/KxSMBProvider.h>
